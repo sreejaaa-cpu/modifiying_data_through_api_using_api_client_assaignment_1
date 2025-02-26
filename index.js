3722eb1b6e203b54cf97c6795b62d8bbaf9d03b1
sreejaaa-cpu <kommaraju.sreeja.s81@kalvium.community>
2025-02-26 11:15:47 +0530

    done

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const MenuItem = require('./schema');
-
+const dotenv = require("dotenv"); 
 const app = express();
+dotenv.config()
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://komarrajusreeja:gYvzzTH8EuVzs7Yl@cluster0.n9mbj.mongodb.net/s81_Rental_housing_finder?retryWrites=true&w=majority&appName=Cluster0',{
+mongoose.connect(process.env.mongo_url)
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
 
+
+app.get("/",(req,res)=>{
+  res.send("hello")
 })
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
 
 app.post('/menu', async (req, res) => {
   try {
     const newMenuItem = new MenuItem(req.body);
+    console.log(newMenuItem)
     const savedMenuItem = await newMenuItem.save();
-    res.status(201).json({ message: 'Menu item created successfully', data: savedMenuItem });
+    res.status(201).json({ message: 'Menu item created successfully' });
   } catch (error) {
     res.status(400).json({ message: 'Error creating menu item', error: error.message });
   }
@@ -23,15 +28,15 @@ app.post('/menu', async (req, res) => {
 
 app.get('/menu', async (req, res) => {
   try {
-    const menuItems = await MenuItem.find({});
+    const menuItems = await MenuItem.find();
     res.json({message:"data retrieved",data:menuItems});
   } catch (error) {
     res.status(500).json({ message: 'Error fetching menu items', error: error.message });
   }
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.port 
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${process.env.port}`));
 
 
 
